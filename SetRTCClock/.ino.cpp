997377ff4dadde0845e056421d9fa9f3f0147716
Jacob Barsøe Kjærgaard <jacob.kjaergaard@prevas.dk>
2015-09-14 21:26:29 +0200

    SetRTCClock: add new project for setting time

---
+++ SetRTCClock/.ino.cpp
@@ -0,0 +1,19 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-09-14 21:22:28
+
+#include "Arduino.h"
+#include <DS3232RTC.h>
+#include <Streaming.h>
+#include <Time.h>
+#include <Wire.h>
+void setup(void) ;
+void loop(void) ;
+void printDate(time_t t) ;
+void printDateTime(time_t t) ;
+void printTime(time_t t) ;
+void printI00(int val, char delim) ;
+
+#include "SetRTCClock.ino"
+
