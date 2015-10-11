e6d4e414e7fb216f1633c0459e6cddfa5b950443
Jacob Barsøe Kjærgaard <jacob.kjaergaard@prevas.dk>
2015-10-11 21:43:15 +0200

    RF24_elmaaler: clean up

--- RF24_sensor_elmaaler/.ino.cpp
+++
@@ -1,8 +0,0 @@
-//This is a automatic generated file
-//Please do not modify this file
-//If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-09-10 21:36:58
-
-#include "Arduino.h"
-
-#include "RF24_sensor_elmaaler.ino"
